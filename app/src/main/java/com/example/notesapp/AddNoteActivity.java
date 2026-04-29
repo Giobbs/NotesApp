715f665d6b202925ad6b07ff0df9fad64e1ecc44
Giobbs <luc.florio@libero.it>
2026-04-29 14:32:45 +0200

    fix AddNoteActivity

--- app/src/main/java/com/example/notesapp/AddNoteActivity.java
+++ app/src/main/java/com/example/notesapp/AddNoteActivity.java
@@ -1,20 +1,23 @@
 package com.example.notesapp;
 
 import android.os.Bundle;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.notesapp.data.local.Note;
+import com.example.notesapp.security.CryptoManager;
 import com.example.notesapp.ui.main.NotesViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddNoteActivity extends AppCompatActivity {
 
     private EditText etTitle, etContent;
-    private Button btnSave;
+    private Switch switchProtect;
+    private FloatingActionButton btnSave;
 
     private NotesViewModel viewModel;
 
@@ -26,18 +29,21 @@ public class AddNoteActivity extends AppCompatActivity {
         // UI
         etTitle = findViewById(R.id.etTitle);
         etContent = findViewById(R.id.etContent);
+        switchProtect = findViewById(R.id.switchProtect);
         btnSave = findViewById(R.id.btnSave);
 
         // ViewModel
         viewModel = new ViewModelProvider(this).get(NotesViewModel.class);
 
-        // click salva
+        // Save
         btnSave.setOnClickListener(v -> saveNote());
     }
 
     private void saveNote() {
+
         String title = etTitle.getText().toString().trim();
         String content = etContent.getText().toString().trim();
+        boolean isProtected = switchProtect.isChecked();
 
         if (title.isEmpty() && content.isEmpty()) {
             Toast.makeText(this, "Inserisci qualcosa", Toast.LENGTH_SHORT).show();
@@ -46,11 +52,19 @@ public class AddNoteActivity extends AppCompatActivity {
 
         Note note = new Note();
         note.setTitle(title);
-        note.setProtected(false);
-        note.setContent(content);
-         viewModel.insert(note, () -> runOnUiThread(() -> {
+        note.setProtected(isProtected);
+
+        if (isProtected) {
+            note.encryptedContent = CryptoManager.encrypt(content);
+            note.setContent("");
+        } else {
+            note.setContent(content);
+            note.encryptedContent = null;
+        }
+
+        viewModel.insert(note, () -> runOnUiThread(() -> {
             Toast.makeText(this, "Nota salvata", Toast.LENGTH_SHORT).show();
-            finish(); // torna alla lista
+            finish();
         }));
     }
 }
